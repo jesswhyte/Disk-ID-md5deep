4ec48eacd4d9bada77d8a58c9dd5349837a61977
jesswhyte <jess.whyte@gmail.com>
2019-04-08 10:16:45 -0400

    added iso to disk-img-extraction

---
+++ disk-img-extraction.sh
@@ -0,0 +1,92 @@
+#! /bin/bash
+#script to id filesystem type for raw disk images using disktype, mount accordingly (currently only hfs and vfat),
+#run sf to create .csv reports, 
+#use rsync to extract files to /Extracted folder
+
+#assumes /mnt/diskid exists
+#assumes $DIR/Extracted exists
+#assumes disktype, hashdeep are installed
+
+### TODO: fix directory checks/creation
+### TODO: fix logging - e.g. if mount fails or extract empty, note in log
+
+#make cwd a variable
+CWD=$(pwd)
+echo $CWD
+
+
+echo "You need the right permissions to run this script, requires disk mounting"
+
+#iterate for every .img file (warning: recursive, limit depth on find with -maxdepth)
+##for FILE in *.img
+for FILE in $(find . -name '*.img');
+do
+#make variable SYSTEM based on a grep of disktype's output
+	SYSTEM=$(disktype "$FILE" | grep "file system")
+	printf "File: %s , System: %s\n" "$FILE" "$SYSTEM" 
+#make variable DIR based on directory containing $FILE
+	DIR=$(dirname $(realpath $FILE))
+
+#if SYSTEM contains FAT
+	if [[ $SYSTEM == "FAT"* ]]
+	then
+#mounts the image in order to run md5deep, note the mount command 
+		sudo mount -t vfat -o loop,ro,noexec $FILE /mnt/diskid/
+#just verify it mounted
+		echo $?
+#cd to mount directory,
+#IMPORTANT NOTE:::: assumes /mnt/diskid exists
+		cd /mnt/diskid
+#use hashdeep or siegfried to create .csv for use by archivists OR dfxmls (comment out the one you don't want)
+		sf -csv -hash md5 . > $DIR"/"$(basename ${FILE%".img"})"-manifest.csv" 
+		#hashdeep -r -l -d -c md5 ./* > $DIR"/"$(basename ${FILE%".img"})"-dfxml.xml"
+#rsync -a all files to $/DIR/Extracted/
+		mkdir "$DIR""/Extracted"
+		rsync -av ./ "$DIR""/Extracted/"
+#cd back to cwd and unmount
+		cd "$CWD"
+		sudo umount /mnt/diskid		
+#if SYSTEM contains HFS
+	elif [[ $SYSTEM == "HFS"* ]]
+	then 
+#mount the image according to forensicwiki mounting suggestions
+		sudo mount -t hfs -o loop,ro,noexec $FILE /mnt/diskid/
+#just verify it mounted
+		echo $?
+#cd to mount directory
+		cd /mnt/diskid
+#use siegfried or hashdeep to create DFXML and .csv files OR dfxmls (comment out the one you don't want)
+		sf -csv -hash md5 . > $DIR"/"$(basename ${FILE%".img"})"-manifest.csv"
+		#hashdeep -r -l -d -c md5 ./* > $DIR"/"$(basename ${FILE%".img"})"-dfxml.xml"
+#rsync -a all files to $DIR/Extracted/
+#add -n flag to test dry run
+		mkdir "$DIR""/Extracted"
+		rsync -av ./ "$DIR""/Extracted/" 
+#cd back to cwd and unmount
+		cd "$CWD"
+		sudo umount /mnt/diskid	
+	fi
+done
+		
+for FILE in $(find ./ -name '*.iso')
+do
+	SYSTEM=$(disktype "$FILE" | grep "file system")	
+	printf "File: %s , System: %s\n\n" "$FILE" "$SYSTEM"
+
+	DIR=$(dirname $(realpath $FILE))
+	mkdir "$DIR""/Extracted"	
+	sudo mount -o loop,ro,noexec $FILE /mnt/diskid/
+	echo $?
+	cd /mnt/diskid
+	#use siegfried or hashdeep to create .csv files OR dfxmls (comment out the one you don't want)
+	sf -csv -hash md5 . > $DIR"/"$(basename ${FILE%".iso"})"-manifest.csv"
+	#hashdeep -r -l -d -c md5 ./* > $DIR"/"$(basename ${FILE%".iso"})"-dfxml.xml"
+	#fiwalk -f -X $DIR"/"$(basename ${FILE%".iso"})"-ISO-dfxml.xml" "$FILE"
+#add -n flag to test dry run
+	rsync -av ./ "$DIR""/Extracted/" 
+#cd back to cwd and unmount
+	cd "$CWD"
+	sudo umount /mnt/diskid	
+
+done
+
